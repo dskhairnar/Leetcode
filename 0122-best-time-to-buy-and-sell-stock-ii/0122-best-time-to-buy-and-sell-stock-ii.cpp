dfbabba899ba3a9011df5cdad1f024ed81f1f030
Dinesh Santosh Khairnar <96805747+Dineshsk12@users.noreply.github.com>
2024-07-28 18:41:31 +0530

    Time: 3 ms (88.54%), Space: 15.9 MB (39.81%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        if(prices.size() == 0) return 0;
-        
-        int ans = 0;
-        int start = prices[0], end = prices[0];
-        
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] > end){
-                //still in the old session
-                //update old end
-                end = prices[i];
-            }else{
-                //sell
-                ans += (end - start);
-                //restart a session
-                start = prices[i];
-                end = prices[i];
-            }
-        }
-        
-        if(end > start) ans += (end - start);
-        
-        return ans;
+       ios_base::sync_with_stdio(false);
+       cin.tie(NULL);
+
+       int n = prices.size();
+       int diff = 0;
+       for(int i =1; i<n; i++){
+        if(prices[i] > prices[i-1])
+        diff+=prices[i]-prices[i-1];
+       }
+       return diff;
     }
 };
