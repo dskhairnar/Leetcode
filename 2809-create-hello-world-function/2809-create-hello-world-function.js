f34b609f9aef2c513de87712f5cd46ff50baaf8e
Dinesh Santosh Khairnar <96805747+dskhairnar@users.noreply.github.com>
2025-03-01 11:58:01 +0530

    Time: 37 ms (95.28%), Space: 54.9 MB (5.99%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -2,9 +2,8 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";  // Return the desired string
+    return function(...args){
+        return "Hello World";  
     }
 };
 
